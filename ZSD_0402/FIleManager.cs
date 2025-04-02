c1390b77cec3ef36ec17d992d6c4a2d74f14c93b
tallosi.milan <tallosi.milan@C19-16>
2025-04-02 08:45:41 +0200

    Add project files.

---
+++ ZSD_0402/FIleManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace ZSD_0402
+{
+    class FileManager
+    {
+        private string fileName;
+        public FileManager(string fileName)
+        {
+            this.fileName = fileName;
+        }
+        public List<Person> ReadFile()
+        {
+            List<Person> lista = new List<Person>();
+            try
+            {
+                StreamReader read = new StreamReader(fileName, Encoding.UTF8);
+                read.ReadLine();
+                while (!read.EndOfStream)
+                {
+                    lista.Add(new Person(read.ReadLine()));
+                }
+                read.Close();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message, "Hiba!");
+            }
+            return lista;
+        }
+
+        public void WriteFile(List<Person> lista)
+        {
+            try
+            {
+                StreamWriter write = new StreamWriter(fileName, false, Encoding.UTF8);
+                write.WriteLine("Első sor");
+                foreach (Person item in lista)
+                {
+                    write.WriteLine(item.Height + "-" + item.Weight);
+                }
+                write.Close();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message, "Hiba!");
+            }
+        }
+    }
+}
---
+++ ZSD_0402/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZSD_0402
+{
+    class Person
+    {
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public Person(string line)
+        {
+            string[] temp = line.Split('-');
+            Height = int.Parse(temp[0]);
+            Weight = int.Parse(temp[1]);
+        }
+        public Person(int Height, int Weight)
+        {
+            this.Height = Height;
+            this.Weight = Weight;
+        }
+    }
+}
